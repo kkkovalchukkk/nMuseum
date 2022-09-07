d2a15e4e38a81dea04af12cf54946d97fc4e1bc3
kkkovalchukkk <student.kovalchuk@yandex.ru>
2022-09-07 13:59:33 +0700

    startd responsive

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.first-swiper', {
   direction: 'horizontal',
   loop: true,
   slidesPerView:2,
@@ -27,3 +27,13 @@ breakpoints: {
 }
     
 });
+
+const secondSwiper = new Swiper('.second-swiper', {
+    directional: 'horizontal',
+    slidesPerView:1,
+     scrollbar: {
+        el: '.scrollbar',
+        draggable: true,
+        dragSize: 26,
+  },
+})
